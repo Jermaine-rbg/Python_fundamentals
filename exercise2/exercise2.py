ec7513498622c83f904bc21331bba0f512df847b
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2021-12-25 18:27:09 -0500

    gross pay commit

--- exercise2/exercise2.py
+++ exercise2/exercise2.py
@@ -1,1 +1,8 @@
-print("exercise3")
+# Enter work hours
+wh = input("Enter Hours: ")
+# Enter hourly rate
+hr = input("Enter Rate: ")
+# Calculate gross check, convert strings into numbers by using float
+check = float(wh) * float(hr)
+# Total gross pay
+print("Pay:", check)
