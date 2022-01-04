93c13e4af4f002d1fb03e906fa540f4f1522bf3d
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-01-03 23:28:20 -0500

    Building Functions

---
+++ exercise6/exercise6.py
@@ -0,0 +1,11 @@
+developer = 10
+print("Software Developer")
+
+def print_lyrics():
+    print("You have to put in 10 times the hardwork")
+    print("To become a successful")
+
+print('Yes')
+print_lyrics()
+developer = developer + 1
+print(developer)
