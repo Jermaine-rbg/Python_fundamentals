74fca392969bd72bccf82f1005c2b28ae4d30bcd
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-01-05 08:41:54 -0500

    Python Loops Correction

--- exercise7/exercise7.py
+++ exercise7/exercise7.py
@@ -2,5 +2,5 @@ applications = 5
 while applications > 0 :
     print(applications)
     applications = applications - 1
-    print("Success")
-    print(applications)
+print("Success")
+print(applications)
---
+++ exercise8/exercise8.py
@@ -0,0 +1,18 @@
+num = 0
+tot = 0.0
+while True :
+    sval = input('Enter a number: ')
+    if sval == 'done' :
+        break
+    try:
+        fval = float(sval)
+    except:
+        print('Invaild input')
+    
+    #print(fval)
+    num = num + 1
+    tot = tot + fval
+
+#print('All done')
+print(tot,num,tot/num)
+
