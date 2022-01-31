33094dfda11d5c61f54e66a273d04835c67a7b7b
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-01-30 22:34:57 -0500

    Added and new file in dev branch

---
+++ git.py
@@ -0,0 +1,11 @@
+developer = 3
+print("Software Developer")
+
+def print_lyrics():
+    print("You have to put in 3 times the hardwork")
+    print("To become a successful")
+
+print('Yes')
+print_lyrics()
+developer = developer + 1
+print(developer)
