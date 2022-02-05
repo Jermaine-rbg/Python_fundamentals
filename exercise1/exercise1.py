0e54bba5beda3e7576988f1fac4f657c644e6d26
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-04 20:33:57 -0500

    mulitple print statements

--- exercise1/exercise1.py
+++ exercise1/exercise1.py
@@ -1,5 +1,11 @@
 # this is a comment in python with the number sign
 
-# this class assignment print statments
+# this class assignment print statment
 
 print('Hello JTC')
+
+# multiple print statements
+
+print('software')
+print('engineer')
+print('is hard work')
