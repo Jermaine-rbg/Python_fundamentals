7e04d8b41cae7774c32d0795176906f7d45aadd0
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-05 19:24:17 -0500

    arithmetic operators

--- exercise2/intergers.py
+++ exercise2/intergers.py
@@ -1,23 +1,28 @@
 # a integer can be a positive or negeitive number
 
-# a positive number
-print(5)
+# # a positive number
+# print(5)
 
-# a negitive number
-print(-5)
+# # a negitive number
+# print(-5)
 
-# or a integer could be zero
-print(0)
+# # or a integer could be zero
+# print(0)
 
-# arithmetic operators
-# addition +
-print(1+3)
-# subtraction -
-print(2-3)
-# multiplcation * 
-print(3*4)
-# division /
-print(4/2)
-# exponents **
-print(5**2)
+# # arithmetic operators
+# # addition +
+# print(1+3)
+# # subtraction -
+# print(2-3)
+# # multiplcation * 
+# print(3*4)
+# # division /
+# print(4/2)
+# # exponents **
+# print(5**2)
 
+# asigning a number a value 
+
+positive_number = 10
+negative_number = -5
+print(positive_number + negative_number)
