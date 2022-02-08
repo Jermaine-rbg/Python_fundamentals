cbfb3cdcd5487e9da21a2cccb7022068b958bf34
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-08 17:59:13 -0600

    strings

---
+++ exercise4/strings.py
@@ -0,0 +1,28 @@
+# assigning variables
+# updating variables
+# integers and floats
+
+# class tonight:
+# strings
+# boolean 
+# convert integers and floats
+
+# strings
+
+address = 'atlanta ave'
+
+# element of a string inside qoutation marks 
+# sinlge or double qoutes 
+
+print('atlanta ave')
+# direct print statement
+
+print(address)
+
+# string concatenation
+
+zipcode = 30313
+city = 'Atlanta'
+state = 'Ga'
+
+print(address, zipcode, city, state)
