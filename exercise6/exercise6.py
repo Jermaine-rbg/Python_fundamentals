8882741edf95e43ef2bdea6012f9ca66ab41c56b
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-13 18:17:53 -0500

    setting variables in a list

--- exercise6/exercise6.py
+++ exercise6/exercise6.py
@@ -1,16 +1,23 @@
 # introduction to list 
 
 
-grocery_list = ["apples", "pasta", "milk"]
-print(grocery_list)
+# grocery_list = ["apples", "pasta", "milk"]
+# print(grocery_list)
 
-integer_list = [60, 93, 723]
-float_list = [60.0, 93.5, 72.3]
-boolean_list = [True, False]
+# integer_list = [60, 93, 723]
+# float_list = [60.0, 93.5, 72.3]
+# boolean_list = [True, False]
 
-mixed_list = [60, 723, 'rollin six owe']
+# mixed_list = [60, 723, 'rollin six owe']
 
-print(integer_list)
-print(float_list)
-print(boolean_list)
-print(mixed_list)
+# print(integer_list)
+# print(float_list)
+# print(boolean_list)
+# print(mixed_list)
+
+item_one = 'apples'
+item_two = 'pasta'
+item_three = 'milk'
+
+grocery_list_two = [item_one, item_two, item_three]
+print(grocery_list_two)
