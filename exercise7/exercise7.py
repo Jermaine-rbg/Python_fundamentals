8f34c02bc6a74e1dbeca626982acaeaf1e29f6f4
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-14 19:01:11 -0500

    list update

--- exercise7/exercise7.py
+++
@@ -1,6 +0,0 @@
-applications = 5
-while applications > 0 :
-    print(applications)
-    applications = applications - 1
-print("Success")
-print(applications)
---
+++ exercise7/list.py
@@ -0,0 +1,19 @@
+# what is a list
+
+our_list = [] # this is a empty list
+
+our_list = [42, 'apple', True]
+# a list can consist of a integer, string and boolean
+print(our_list)
+print(type(our_list))
+
+groceries = ['apples', 'eggs', 'pasta', 'carrots']
+float_list = [12.4, 5.8, 2.9, 55.6]
+boolean_list = [True, False, True, False, True]
+
+print(groceries)
+print(float_list)
+print(boolean_list)
+
+mixed_list =  [groceries, float_list, boolean_list]
+print(mixed_list)
