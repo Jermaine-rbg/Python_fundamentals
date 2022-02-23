19ff3c3204077bbdb9f3712a5d76a652e3f5dc70
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-22 21:19:27 -0500

    functions

--- exercise9/functions.py
+++ exercise9/functions.py
@@ -16,3 +16,16 @@ def say_hello(name):
 say_hello('Jermaine')
 say_hello('Tony')
 say_hello('Jaxen')
+
+
+def times_two(num):
+    print(num + 2)
+
+times_two(3)
+times_two(5.0)
+
+def multiply(a,b):
+    print(a + b)
+
+multiply(5,4)
+multiply(6.0, 100)
